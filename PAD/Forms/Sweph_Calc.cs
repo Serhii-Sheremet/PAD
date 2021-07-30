fdd9f6ffda84d475d1a8dbac49fedeb6b17141eb
AlanRed <ssheremet@ukr.net>
2021-07-30 10:49:29 +0200

    sweph calculation - in progress

--- PAD/Forms/Sweph_Calc.cs
+++ PAD/Forms/Sweph_Calc.cs
@@ -30,103 +30,67 @@ namespace PAD
             List<PlanetData> planetDataList = new List<PlanetData>();
             int cZnak = 0, cNakshatra = 0, cPada = 0;
             string cRetro = string.Empty;
-            int monthChange = 0, dayChange = 0, hourChange = 0, minuteChange = 0, secondChange = 0;
+            DateTime dateChange;
 
             while (curDate.Year < yearTo)
             {
-                monthChange = CheckChangeOfMonth(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                if (monthChange != curDate.Month)
+                TimeSpan tsStep = curDate.AddMonths(+1).Subtract(curDate);
+                dateChange = CheckChangeInTimePeriod(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, tsStep, out calcRes);
+                if (dateChange.Month != curDate.Month)
                 {
-                    curDate = new DateTime(curDate.Year, (monthChange - 1), curDate.Day, curDate.Hour, curDate.Minute, curDate.Second);
-                    dayChange = CheckChangeOfDay(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                    if (dayChange == 0)
+                    tsStep = curDate.AddDays(+1).Subtract(curDate);
+                    dateChange = CheckChangeInTimePeriod(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, tsStep, out calcRes);
+                    if (dateChange.Day != curDate.Day)
                     {
-                        curDate = new DateTime(curDate.Year, monthChange, curDate.Day, curDate.Hour, curDate.Minute, curDate.Second);
-                        dayChange = CheckChangeOfDay(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                    }
-                    else
-                    {
-                        if (dayChange != curDate.Day)
+                        curDate = new DateTime(dateChange.Year, dateChange.Month, dateChange.AddDays(-1).Day, curDate.Hour, curDate.Minute, curDate.Second);
+                        tsStep = curDate.AddHours(+1).Subtract(curDate);
+                        dateChange = CheckChangeInTimePeriod(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, tsStep, out calcRes);
+
+                        if (dateChange.Hour != curDate.Hour)
                         {
-                            curDate = new DateTime(curDate.Year, curDate.Month, (dayChange - 1), curDate.Hour, curDate.Minute, curDate.Second);
-                            hourChange = CheckChangeOfHour(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                            if (hourChange == 0)
+                            curDate = new DateTime(dateChange.Year, dateChange.Month, dateChange.Day, dateChange.AddHours(-1).Hour, curDate.Minute, curDate.Second);
+                            tsStep = curDate.AddMinutes(+1).Subtract(curDate);
+                            dateChange = CheckChangeInTimePeriod(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, tsStep, out calcRes);
+
+                            if (dateChange.Minute != curDate.Minute)
                             {
-                                curDate = new DateTime(curDate.Year, curDate.Month, dayChange, curDate.Hour, curDate.Minute, curDate.Second);
-                                hourChange = CheckChangeOfHour(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                            }
-                            else
-                            {
-                                if (hourChange != curDate.Hour)
+                                curDate = new DateTime(dateChange.Year, dateChange.Month, dateChange.Day, dateChange.Hour, dateChange.AddMinutes(-1).Minute, curDate.Second);
+                                tsStep = curDate.AddSeconds(+1).Subtract(curDate);
+                                dateChange = CheckChangeInTimePeriod(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, tsStep, out calcRes);
+
+                                curDate = dateChange;
+                                currentZnak = GetCurrentZnak(calcRes[0]);
+                                currentNakshatra = GetCurrentNakshatra(calcRes[0]);
+                                currentPada = GetCurrentPada(calcRes[0]);
+                                currentRetro = cRetro;
+
+                                PlanetData pdTemp = new PlanetData
                                 {
-                                    curDate = new DateTime(curDate.Year, curDate.Month, curDate.Day, (hourChange - 1), curDate.Minute, curDate.Second);
-                                    minuteChange = CheckChangeOfMinute(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                                    if (minuteChange == 0)
-                                    {
-                                        curDate = new DateTime(curDate.Year, curDate.Month, curDate.Day, hourChange, curDate.Minute, curDate.Second);
-                                        minuteChange = CheckChangeOfMinute(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                                    }
-                                    else
-                                    {
-                                        if (minuteChange != curDate.Minute)
-                                        {
-                                            curDate = new DateTime(curDate.Year, curDate.Month, curDate.Day, curDate.Hour, (minuteChange - 1), curDate.Second);
-                                            secondChange = CheckChangeOfSecond(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                                            if (secondChange == 0)
-                                            {
-                                                curDate = new DateTime(curDate.Year, curDate.Month, curDate.Day, curDate.Hour, minuteChange, curDate.Second);
-                                                secondChange = CheckChangeOfSecond(eCalc, longitude, latitude, altitude, currentZnak, currentNakshatra, currentPada, currentRetro, curDate, out calcRes);
-                                            }
-                                            else
-                                            {
-                                                if (secondChange != curDate.Second)
-                                                {
-                                                    curDate = new DateTime(curDate.Year, curDate.Month, curDate.Day, curDate.Hour, curDate.Minute, secondChange);
-
-                                                    currentZnak = GetCurrentZnak(calcRes[0]);
-                                                    currentNakshatra = GetCurrentNakshatra(calcRes[0]);
-                                                    currentPada = GetCurrentPada(calcRes[0]);
-                                                    currentRetro = cRetro;
-
-                                                    PlanetData pdTemp = new PlanetData
-                                                    {
-                                                        Date = curDate,
-                                                        Longitude = calcRes[0],
-                                                        Latitude = calcRes[1],
-                                                        SpeedInLongitude = calcRes[3],
-                                                        SpedInLatitude = calcRes[4],
-                                                        Retro = cRetro,
-                                                        ZodiakId = cZnak,
-                                                        NakshatraId = cNakshatra,
-                                                        PadaId = cPada
-                                                    };
-
-                                                    planetDataList.Add(pdTemp);
-
-                                                    
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                }
+                                    Date = curDate,
+                                    Longitude = calcRes[0],
+                                    Latitude = calcRes[1],
+                                    SpeedInLongitude = calcRes[3],
+                                    SpedInLatitude = calcRes[4],
+                                    Retro = cRetro,
+                                    ZodiakId = cZnak,
+                                    NakshatraId = cNakshatra,
+                                    PadaId = cPada
+                                };
+                                planetDataList.Add(pdTemp);
                             }
                         }
                     }
                 }
-
                 curDate = curDate.AddSeconds(+1);
             }
-
             label1.Text = "Count in a list: " + planetDataList.Count;
         }
 
-        private int CheckChangeOfMonth(EpheCalculation eCalc, double longitude, double latitude, double altitude, int currentZnak, int currentNakshatra, int currentPada, string currentRetro, DateTime curDate, out double[] calcRes)
+        private DateTime CheckChangeInTimePeriod(EpheCalculation eCalc, double longitude, double latitude, double altitude, int currentZnak, int currentNakshatra, int currentPada, string currentRetro, DateTime curDate, TimeSpan tsStep, out double[] calcRes)
         {
             int cZnak = 0, cNakshatra = 0, cPada = 0;
             string cRetro = string.Empty;
             calcRes = new double[6];
-            int monthChange = 0;
 
             for (DateTime date = curDate; date < curDate.AddYears(+1);)
             {
@@ -139,121 +103,12 @@ namespace PAD
 
                 if (cZnak != currentZnak)
                 {
-                    monthChange = date.Month;
-                    break;
+                    return date;
                 }
 
-                date = date.AddMonths(+1);
+                date = date.Add(tsStep);
             }
-            return monthChange;
-        }
-
-        private int CheckChangeOfDay(EpheCalculation eCalc, double longitude, double latitude, double altitude, int currentZnak, int currentNakshatra, int currentPada, string currentRetro, DateTime curDate, out double[] calcRes)
-        {
-            int cZnak = 0, cNakshatra = 0, cPada = 0;
-            string cRetro = string.Empty;
-            calcRes = new double[6];
-            int dayChange = 0;
-
-            for (DateTime date = curDate; date < curDate.AddMonths(+1);)
-            {
-                calcRes = eCalc.SWE_Calculation(EpheConstants.SE_SUN, date, longitude, latitude, altitude);
-
-                cZnak = GetCurrentZnak(calcRes[0]);
-                cNakshatra = GetCurrentNakshatra(calcRes[0]);
-                cPada = GetCurrentPada(calcRes[0]);
-                cRetro = string.Empty;
-
-                if (cZnak != currentZnak)
-                {
-                    dayChange = date.Day;
-                    break;
-                }
-
-                date = date.AddDays(+1);
-            }
-            return dayChange;
-        }
-
-        private int CheckChangeOfHour(EpheCalculation eCalc, double longitude, double latitude, double altitude, int currentZnak, int currentNakshatra, int currentPada, string currentRetro, DateTime curDate, out double[] calcRes)
-        {
-            int cZnak = 0, cNakshatra = 0, cPada = 0;
-            string cRetro = string.Empty;
-            calcRes = new double[6];
-            int hourChange = 0;
-
-            for (DateTime date = curDate; date < curDate.AddDays(+1);)
-            {
-                calcRes = eCalc.SWE_Calculation(EpheConstants.SE_SUN, date, longitude, latitude, altitude);
-
-                cZnak = GetCurrentZnak(calcRes[0]);
-                cNakshatra = GetCurrentNakshatra(calcRes[0]);
-                cPada = GetCurrentPada(calcRes[0]);
-                cRetro = string.Empty;
-
-                if (cZnak != currentZnak)
-                {
-                    hourChange = date.Hour;
-                    break;
-                }
-
-                date = date.AddHours(+1);
-            }
-            return hourChange;
-        }
-
-        private int CheckChangeOfMinute(EpheCalculation eCalc, double longitude, double latitude, double altitude, int currentZnak, int currentNakshatra, int currentPada, string currentRetro, DateTime curDate, out double[] calcRes)
-        {
-            int cZnak = 0, cNakshatra = 0, cPada = 0;
-            string cRetro = string.Empty;
-            calcRes = new double[6];
-            int minuteChange = 0;
-
-            for (DateTime date = curDate; date < curDate.AddHours(+1);)
-            {
-                calcRes = eCalc.SWE_Calculation(EpheConstants.SE_SUN, date, longitude, latitude, altitude);
-
-                cZnak = GetCurrentZnak(calcRes[0]);
-                cNakshatra = GetCurrentNakshatra(calcRes[0]);
-                cPada = GetCurrentPada(calcRes[0]);
-                cRetro = string.Empty;
-
-                if (cZnak != currentZnak)
-                {
-                    minuteChange = date.Minute;
-                    break;
-                }
-
-                date = date.AddMinutes(+1);
-            }
-            return minuteChange;
-        }
-
-        private int CheckChangeOfSecond(EpheCalculation eCalc, double longitude, double latitude, double altitude, int currentZnak, int currentNakshatra, int currentPada, string currentRetro, DateTime curDate, out double[] calcRes)
-        {
-            int cZnak = 0, cNakshatra = 0, cPada = 0;
-            string cRetro = string.Empty;
-            calcRes = new double[6];
-            int secondChange = 0;
-
-            for (DateTime date = curDate; date < curDate.AddMinutes(+1);)
-            {
-                calcRes = eCalc.SWE_Calculation(EpheConstants.SE_SUN, date, longitude, latitude, altitude);
-
-                cZnak = GetCurrentZnak(calcRes[0]);
-                cNakshatra = GetCurrentNakshatra(calcRes[0]);
-                cPada = GetCurrentPada(calcRes[0]);
-                cRetro = string.Empty;
-
-                if (cZnak != currentZnak)
-                {
-                    secondChange = date.Second;
-                    break;
-                }
-
-                date = date.AddSeconds(+1);
-            }
-            return secondChange;
+            return curDate;
         }
 
         private int GetCurrentZnak(double longitude)
